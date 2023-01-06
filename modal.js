c74ac2e136667459e0d4a5bc8877eb1a99f81f73
Adman59 <adrien.magnies@hotmail.fr>
2023-01-06 13:33:18 +0100

    Optimisation du HTML + fermeture de la modal en js

--- modal.js
+++ modal.js
@@ -7,17 +7,31 @@ function editNav() {
   }
 }
 
-// DOM Elements
+//---------------DOM ELEMENTS-----------------//
+
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeIconModal = document.querySelectorAll('#closeIcon');
 
-// launch modal event
+const = "test"
+//---------------EVENTS-----------------//
+
+// OPEN modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// CLOSE modal event
+closeIconModal.forEach((btn) => btn.addEventListener("click", closeModal));
+
+
+//---------------FONCTIONS-----------------//
+
+// OPEN modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// CLOSE modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
